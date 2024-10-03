fbf07cfbaf7cc1ba9e971479d1ef810cca99e225
Joe DF <3848219+joedf@users.noreply.github.com>
2024-10-03 14:47:59 -0400

    First commit to finally publish has-rs

---
+++ README.md
@@ -0,0 +1,15 @@
+# has-rs
+
+A simple implementation of [`has`](https://github.com/kdabir/has) by [kdabir](https://github.com/kdabir).
+> `has` checks presence of various command line tools on the PATH and reports their installed version.
+
+This was created as part of [my practice and learning](https://github.com/joedf/LearningRust) of the Rust programming language. I started this back in August 2022, but I got to releasing this now (October 2024).
+
+## build & run
+
+You can simply navigate to the `has-rs` folder and run the following command to build it:
+> cargo build
+
+... or to run it and check if a system has `git`, `gcc` and `node` installed:
+> cargo run git gcc node
+
---
+++ has-rs/src/main.rs
@@ -0,0 +1,83 @@
+use std::env;
+use std::process::Command;
+use lazy_static::lazy_static;
+use regex::Regex;
+use which::which;
+
+// tested with: cargo run tere ls node npm nope git java rustc rustup cargo
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	
+	if args.len() > 1 {
+		for app in args.iter().skip(1) {
+			let v = get_version(app);
+			if v.len() > 0 {
+				println!("✔ {} {}", app, v);
+			} else {
+				println!("✘ {}", app);
+			}
+		}
+	} else {
+		println!("has-rs v0.1.0\n\
+		---------------------------------------------\n\
+		written by joedf\n\
+		released under the MIT License\n\
+		based on https://github.com/kdabir/has");
+	}
+}
+
+fn get_version(app: &str) -> String {
+	// accounts for PATHEXT on MS Windows!
+	let resolved_path = match which(app) {
+		Ok(result) => result.into_os_string().into_string().unwrap(),
+		Err(_) => String::new()
+	};
+
+	// println!("{}", resolved_path);
+
+	if resolved_path.len() <= 0 {
+		return String::new()
+	}
+
+	let mut command_exists = false;
+
+	lazy_static! { static ref RE: Regex = Regex::new(r"(?i)(\d+\.|v){1,3}\d+").unwrap(); }
+
+	for arg in ["--version", "-version", "-V", "-v", "version"] {
+		let output = try_get_version(resolved_path.to_string(), arg);
+
+		// println!("{}", output);
+
+		if output.len() > 0 {
+			let version = match RE.captures(&output) {
+				Some(x) => x.get(0).unwrap().as_str().to_string().replace("v",""),
+				None => String::new()
+			};
+
+			if version.len() > 0 {
+				return version
+			}
+
+			command_exists = true;
+		}
+	}
+
+	if command_exists {
+		return "n/a".to_string()
+	} else {
+		return String::new()
+	}
+}
+
+fn try_get_version(app: String, arg: &str) -> String {
+	let output = match Command::new(app)
+		.arg(arg)
+		.output() {
+			Ok(s) => format!("{} {}",
+				String::from_utf8(s.stdout).unwrap(),
+				String::from_utf8(s.stderr).unwrap()),
+			Err(_) => return String::new()
+	};
+
+	return output
+}
